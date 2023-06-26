18c2e5cc3b09f4ea564a8b1c861264838134f2d8
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-06-26 01:11:34 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const name = "jeannine";
+	const url = "https://www.nutritionfacts.org";
+	const linkText = "Nutrition Facts with Dr. Greger";
+	const element = (
+		<p>
+			{" "}
+			Hey, {name}! Please visit <a href={url}>{linkText}</a>.
+		</p>
+	);
+	return element;
 }
-
 export default App;
